0d5d4404c1b09af241271bdd6d20e5ebaddb7672
José Rafael Zarembski Braga <cadastro@jrzbraga.com>
2014-05-20 21:59:16 -0300

    Update login.js
    Alterado o método $POST para $AJAX, para que haja suporte de error retornado do servidor.

--- js/login.js
+++ js/login.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
-	$("button#btnLogin").click( function() {
-	 	if(( $("#inputEmail").val() == "") || ($("#inputPassword").val() == "" ))
+	$("#btnLogin").click( function() {
+	 	if( ($("#inputEmail").val() == "") || ($("#inputPassword").val() == "") )
 	 		$("#formLogin div").addClass('has-error');
-		else if {
-			// Condição caso não há usuário existente
-		} else {
-			$.post( $("#formLogin").attr("action"), $("#formLogin :input").serializeArray(), function(data) {
-					$("#pLogout").html(data);
-					$("#formLogin").hide(400);
-					$("#divSignup").hide(400);
-					$('#formLogout').show(400);
-			});
-		 
-			$("#formLogin").submit( function() {
-				return false;	
+		else {
+			$.ajax({
+				  type: "POST",
+				  url: $("#formLogin").attr("action"),
+				  data: $("#formLogin :input").serializeArray(),
+				  success: function(data){
+				        alert( "Data Saved: " + data );
+				},
+				  error: function(XMLHttpRequest, textStatus, errorThrown) {
+				     alert("some error");
+				}
 			});
 		}
 	});
